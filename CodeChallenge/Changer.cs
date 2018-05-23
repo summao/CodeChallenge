066450207b8beaa24088e5bd01f1de6621e8aeba
Wittawat Timjoy <latung@windowslive.com>
2018-05-23 23:27:37 +0700

    add change

--- CodeChallenge/Changer.cs
+++ CodeChallenge/Changer.cs
@@ -5,9 +5,37 @@ namespace CodeChallenge
 {
     public class Changer
     {
-        public Dictionary<double,double> Change(double price,double paid)
+        public Dictionary<double,int> Change(double price,double paid)
         {
-            throw new NotImplementedException("Please create a test first");
+            if (paid < price || paid == price)
+            {
+                return null;
+            }
+
+            var change = paid - price;
+            Dictionary<double, int> result = new Dictionary<double, int>();
+            var notes = new List<double>
+            {
+                1000D,500D,100D,50D,20D,10D,5D,2D,1D,0.5D,0.25D
+            };
+
+            foreach (var value in notes)
+            {
+                var d = change / value;
+
+                if (d >= 1)
+                {
+                    result.Add(value, (int)d);
+                    change -= (int)d * value;
+                }
+
+                if (change < 0.25 )
+                {
+                    break;
+                }
+            }
+
+            return result;
         }
     }
 }
