1b391c9ffcc3a998c18c7f16ce33f991bccdf157
Wittawat Timjoy <latung@windowslive.com>
2018-05-23 21:57:01 +0700

    init

---
+++ CodeChallenge/Changer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeChallenge
+{
+    public class Changer
+    {
+        public Dictionary<double,double> Change(double price,double paid)
+        {
+            throw new NotImplementedException("Please create a test first");
+        }
+    }
+}
--- CodeChallenge/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CodeChallenge
-{
-    public class Class1
-    {
-    }
-}
